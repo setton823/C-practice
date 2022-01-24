9f573b99135b5de4f7e2c736bd5e0ffa13eb4651
setton823 <97462865+setton823@users.noreply.github.com>
2022-01-24 10:22:40 +0900

    Add files via upload

---
+++ practice9.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class practice9 : MonoBehaviour
+{
+    int[] numbers = { 1, 1, 2, 2 };
+
+    void Start()
+    {
+        Debug.Log(Mean(numbers));
+    }
+
+    float Mean(int[] numbers)
+    {
+        float sum = 0;
+        int count = numbers.Length;
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum / count;     // 合計/個数 
+    }
+}
