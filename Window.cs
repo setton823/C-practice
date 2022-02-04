f1f551689aa7d89a046f84e5f9cb0ffa56a1d50c
setton823 <97462865+setton823@users.noreply.github.com>
2022-02-04 22:47:22 +0900

    Add files via upload

---
+++ Window.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+public class Window
+{
+    public static void Main()
+    {
+        Form fm;
+        fm = new Form();
+
+        fm.Text = "Hello world!!";
+
+        Application.Run(fm);
+    }
+}
